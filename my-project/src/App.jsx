9a861e3b1e0474653b38bb98b1bdb6c4ed581b3b
boldcoder18 <boldurname@gmail.com>
2026-08-28 09:46:09 +0530

    done

---
+++ my-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Section1 from './components/Section0.1/section1'
+
+const App = () => {
+  return (
+    <div>App
+      <Section1 />
+    </div>
+  )
+}
+
+export default App
---
+++ my-project/src/components/Section0.1/section1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './navbar'
+
+const sSection1 = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Section1
