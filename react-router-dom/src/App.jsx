e5d6dbe76fd4a740933808242a0e7bc26cddc862
boldcoder18 <boldurname@gmail.com>
2026-09-02 17:10:10 +0530

    basic of react router dom

---
+++ react-router-dom/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contect from './Pages/Contact'
+import Navbar from './components/Navbar'
+const App = () => {
+  return (
+    <div>
+
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contect />} />
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
